c67688cfa4e6dca736191d859ec083aa7bebe036
kento <19922991kento@gmail.com>
2020-04-22 18:04:34 +0000

    mynews

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+
+});
+    
+Route::group(['prefix' => 'admin'], function() {
+    route::get('news/create', 'Admin\NewsController@add');
+});
+
+
+Route::group(['prefix' => 'admin'], function() {
+    route::get('profile/create', 'Admin\ProfileController@add');
+    route::get('profile/edit', 'Admin\profileController@edit');
+});
