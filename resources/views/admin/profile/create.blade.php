f8f7e903685153e7bdf8d79c3158e19ef6d38421
kento <19922991kento@gmail.com>
2020-04-24 21:00:05 +0000

    10kadai

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-eqiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>Myニュースプロフィール作成画面</h1>
+        <ul>
+          <li>ニックネーム</li>
+          <li>趣味
+          <ol>
+          <li>釣り</li>
+          <li>バイク</li>
+          <li>ダーツ</li>
+          </ol>
+          </li>
+ 　        <li>年齢</li>
+        </ul>
+    </body>
+</html>
